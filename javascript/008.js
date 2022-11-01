098717cf0a83531f04c643a38de17e6a1ae1f319
jk <1289446386@qq.com>
2022-11-01 10:15:44 +0800

    update

--- javascript/008.js
+++ javascript/008.js
@@ -12,11 +12,23 @@ function* getData() {
   const res3 = yield requestData(res2 + "-" + "jimin");
   console.log(res3);
 }
-const generator = getData();
-generator.next().value.then((res) => {
-  generator.next(res).value.then((res) => {
-    generator.next(res).value.then((res) => {
-      generator.next(res);
-    });
-  });
-});
+
+// const generator = getData();
+// generator.next().value.then((res) => {
+//   generator.next(res).value.then((res) => {
+//     generator.next(res).value.then((res) => {
+//       generator.next(res);
+//     });
+//   });
+// });
+
+const co = require("co");
+// co(getData);
+
+async function getData() {
+  const res1 = await requestData("jk");
+  const res2 = await requestData(res1 + "-" + "suga");
+  const res3 = await requestData(res2 + "-" + "jimin");
+  console.log(res3);
+}
+getData();
